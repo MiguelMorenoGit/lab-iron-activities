f8449eb7ddf48a7e92452b8bb0da90044d370970
MiguelMorenoGit <katsura501@gmail.com>
2019-04-18 21:10:11 +0200

    get data with axios done, mdb install

--- src/App.js
+++ src/App.js
@@ -7,7 +7,44 @@ import ActivitieDetail from './pages/ActivitieDetail';
 //importamos el switch y el Route para las rutas que necesitemos
 import { Switch, Route} from 'react-router-dom';
 
+// desde aqui llamaremos a la api
+// hemos instalado axios ahora lo importamos
+import axios from 'axios';
+
 class App extends Component {
+  //ponemos el estado inicial del componente
+  state = {
+    dataActivities:[],
+    dataLoad: "false",
+    itemsPerPage:18,
+    offset:1
+  };
+
+  // funcion que se ejecutara cuando se monte el componente
+  componentDidMount(){
+
+    const {itemsPerPage, offset} = this.state;
+
+    axios.get(`https://api.musement.com/api/v3/venues/164/activities?limit=${itemsPerPage}&offset=${offset}`)
+
+    .then(({data}) => {
+      console.log(data)
+      this.setState({
+        dataActivities:data,
+        dataLoad: "true"
+      })
+    })
+    .catch(err => console.log(err))
+
+    console.log(this.state.dataActivities)
+  }
+
+
+  // listActivities = () => {
+  //   return this.state.dataActivities.map( activity =>)
+  // }
+
+
   render() {
     return (
       <Switch>
